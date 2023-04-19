fa4e47859c7135b09ae831110cbf8a8645d805da
Simon Jäger <simonjager@hotmail.com>
2023-04-19 15:05:37 +0200

    Lade till första testet som prov

--- malmomusik_test.py
+++ malmomusik_test.py
@@ -5,6 +5,8 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.support.wait import WebDriverWait
 from helper_tests_malmomusik import simple_assert, boolean_assert
 
+
+# Constants
 MAIN_SITE = "https://www.malmomusikaffar.com/"
 ELEC_GUIT = "https://www.malmomusikaffar.com/stranginstrument/gitarr/elgitarrer"
 T484 = "https://www.malmomusikaffar.com/eastman-t484-classic-t484"
@@ -22,6 +24,14 @@ def load_driver():
 # Tests
 # Test 1: Öppettider
 def assert_open_time(load_driver):
+
+    driver = load_driver
+
+    driver.get(MAIN_SITE)
+
+    heading = driver.find_element(By.XPATH, "/html/body/div[3]/footer/section/ul/li[1]/header/span")
+
+    simple_assert(heading.text, "M-F: 10-18 L: 10-14")
 
 # Test 2: Hitta Eastman T484
 
